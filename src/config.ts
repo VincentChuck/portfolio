9d93c81d4e61577365f2cd91a61887648064ff99
Vincent Chuck <vincent.chuck7@gmail.com>
2023-08-07 11:30:08 +0800

    Update profile picture

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = "Vincent Chuck";
+export const SITE_DESCRIPTION =
+  "Vincent Chuck is a software developer who is passionate about building great digital experiences.";
